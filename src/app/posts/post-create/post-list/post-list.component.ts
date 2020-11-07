3d3bd70205d3037a96da4d6b07afdd3175bd72a5
Nijat Mursali <nijatmursali@gmail.com>
2020-11-07 22:43:51 +0100

    adding faker into app

--- src/app/posts/post-create/post-list/post-list.component.ts
+++ src/app/posts/post-create/post-list/post-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {image, lorem, name, date} from 'faker';
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html'
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
 export class PostListComponent {
 
   posts = [
-]
+    {title: lorem.word(), authorImg: image.avatar(),author: name.firstName() + ' ' + name.lastName(), authorProfile: '@' + name.firstName().toLowerCase() + name.lastName().toLowerCase(), content: lorem.sentences(), time: date.recent()},
+    {title: lorem.word(), authorImg: image.avatar(),author: name.firstName() + ' ' + name.lastName(), authorProfile: '@' + name.firstName().toLowerCase() + name.lastName().toLowerCase(), content: lorem.sentences(), time: date.recent()},
+    {title: lorem.word(), authorImg: image.avatar(),author: name.firstName() + ' ' + name.lastName(), authorProfile: '@' + name.firstName().toLowerCase() + name.lastName().toLowerCase(), content: lorem.sentences(), time: date.recent()},
+    {title: lorem.word(), authorImg: image.avatar(),author: name.firstName() + ' ' + name.lastName(), authorProfile: '@' + name.firstName().toLowerCase() + name.lastName().toLowerCase(), content: lorem.sentences(), time: date.recent()},
+    {title: lorem.word(), authorImg: image.avatar(),author: name.firstName() + ' ' + name.lastName(), authorProfile: '@' + name.firstName().toLowerCase() + name.lastName().toLowerCase(), content: lorem.sentences(), time: date.recent()},
+  ]
 }
